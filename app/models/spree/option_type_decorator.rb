48e31c7beeb9a62e8709af29ee772e9fb2d6b460
dragsbaek <tdragsbaek@hotmail.com>
2017-10-17 22:43:30 +0200

    ...

--- app/models/spree/option_type_decorator.rb
+++ app/models/spree/option_type_decorator.rb
@@ -2,6 +2,6 @@ Spree::OptionType.class_eval do
 
   scope :color, -> { where(style: 3)}
 
-  enum style: { select: 0, radio: 1, dropdown: 2, color_picker: 3 }
+  enum style: { standard: 0, radio: 1, dropdown: 2, color_picker: 3 }
 
 end
